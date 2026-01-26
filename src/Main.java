73f3605cbe2dd9be1c57c20e7449a2b629ea6e22
Youssef <yousseferrachid@gmail.com>
2026-01-26 15:10:09 +0100

    adding menu of prinsipal , Administrator, Trader

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,54 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    public static void menuPrincipal(){
+        System.out.println("=============================");
+        System.out.println("   Welcome to Xtrade app ");
+        System.out.println("=============================");
+        System.out.println("1. Admin section");
+        System.out.println("2. Trader section");
+        System.out.println("=============================");
+    }
+    public static void controler(int choice){
+        switch(choice){
+            case 1:
+                menuofAdmin();
+                break;
+            case 2:
+                menuofTrader();
+                break;
+                default:
+                    System.out.println("Invalid choice");
         }
+    }
+    public static void menuofAdmin(){
+        System.out.println("============================");
+        System.out.println("      Administrator");
+        System.out.println("============================");
+
+        System.out.println("1. Admin section");
+        System.out.println("2. Trader section");
+
+    }
+    public static void menuofTrader(){
+        System.out.println("============================");
+        System.out.println("          Trader");
+        System.out.println("============================");
+
+        System.out.println("1. Admin section");
+        System.out.println("2. Trader section");
+    }
+    public static int getchoice(String message){
+        System.out.println(message);
+        int choice = sc.nextInt();
+        return choice;
+    }
+
+    public static void main(String[] args) {
+        menuPrincipal();
+        int choice = getchoice("Enter your choice?");
+        controler(choice);
+
     }
 }
